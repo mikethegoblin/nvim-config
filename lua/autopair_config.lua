c892f783f6c65316ebff48de484fdbe319b39424
Zeyu Gu <mikegu0508@gmail.com>
2023-01-16 17:22:17 -0500

    modified autopair config

--- lua/autopair_config.lua
+++ lua/autopair_config.lua
@@ -1,1 +1,9 @@
-require("nvim-autopairs").setup({})
+require("nvim-autopairs").setup({
+	disable_filetype = {"TelescopePrompt", "vim"},
+	check_ts = true,
+    ts_config = {
+        lua = {'string'},-- it will not add a pair on that treesitter node
+        javascript = {'template_string'},
+        java = false,-- don't check treesitter on java
+    },
+})
